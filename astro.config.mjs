62becacbeb1264612d954902aedd7a231e6b564b
Daniel H. Alcojor <dhalcojor@gmail.com>
2024-08-03 12:57:20 +0200

    Add Google Analytics integration
    Integrated @astrolib/analytics library for tracking purposes. Updated package.json, pnpm-lock.yaml, and configurations accordingly. Inserted GoogleAnalytics component into the main layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import libAnalytics from "@astrolib/analytics";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
+    integrations: [tailwind(), libAnalytics()],
+    output: "server",
+    adapter: vercel()
 });
